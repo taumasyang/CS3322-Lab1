1dac0cb1ba87a23975e8865a55b86ec884179fa3
tauyoung <tauyoung@icloud.com>
2023-11-17 23:42:40 +0800

    T4.2

--- answer/scene-4.2.3.sql
+++ answer/scene-4.2.3.sql
@@ -1,1 +1,7 @@
--- 1 条语句：在审计日志中查询
+SELECT * FROM OrderAudit WHERE
+		old_order_id = new_order_id 
+	AND old_create_date = new_create_date
+	AND old_customer_id = new_customer_id
+	AND old_product_id = new_product_id
+	AND old_quantity != new_quantity
+	AND old_is_filled = new_is_filled;
